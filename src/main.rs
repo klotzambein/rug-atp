5d47177b94b2f20ca7288d39c862decb7bc24597
Robin Kock <git@robin-kock.com>
2021-02-26 17:08:51 +0100

    Implement basic tile map

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
-use glium::Surface;
-use gui_framework::{AppInit, canvas::{CanvasError, CanvasObject, DrawingContext}};
+use glium::texture::Texture2d;
+use glium::{Surface, VertexBuffer};
+use gui_framework::{
+    canvas::{CanvasError, CanvasObject, DrawingContext},
+    graphics::primitives::{Sprite, Vf2},
+    texture::load_png_texture,
+    AppInit,
+};
 
 pub struct CanvasGrid {
-
+    vertex_buffer: VertexBuffer<Sprite>,
+    texture: Texture2d,
 }
 
 impl CanvasObject for CanvasGrid {
     fn draw<'a>(&self, ctx: &mut DrawingContext<'a>) -> Result<(), CanvasError> {
-        ctx.programs
-        
+        ctx.programs.draw_sprites(
+            ctx.target,
+            self.vertex_buffer.slice(..).unwrap(),
+            &self.texture,
+            ctx.model_transform,
+            ctx.view_transform,
+        )?;
+
         Ok(())
     }
 }
@@ -37,13 +50,26 @@ fn main() {
     // - - - UI init - - -
     // let mut ui = UI::new(app.imgui.clone());
 
+    let grid = CanvasGrid {
+        vertex_buffer: VertexBuffer::new(
+            &app.display,
+            &(0..64).map(|i| Sprite {
+                vertex: Vf2::new((i % 8) as f32 * 10.0, (i / 8) as f32 * 10.0),
+                size: Vf2::new(10., 10.),
+                texture_index: i,
+            }).collect::<Vec<_>>(),
+        )
+        .unwrap(),
+        texture: load_png_texture(&app.display),
+    };
+
     app.run(move |app, target, last_frame| {
         // state.get_mut().model.room.as_mut().unwrap().step(0.001);
         // state.get_mut().model.redraw_room(&app.display).unwrap();
 
         target.clear_color_srgb(242. / 255., 206. / 255., 223. / 255., 1.);
 
-        app.canvas.draw(target, &root_canvas_obj, &()).unwrap();
+        app.canvas.draw(target, &grid, &()).unwrap();
 
         // ui.draw(last_frame, target, &state, &mut cmd);
         // cmd.poll();
